179173854e644b950a63f4085ea85b36e1064bf6
SirJosh3917 <joshuaneedham@hotmail.com>
2019-11-15 18:33:33 -0500

    do deep searching in RemoveAllUnreferencedMethodsOptimization
    it was counting anything that wasn't referenced in main as unreferenced

--- src/VarCode/Optimizer/Alpha/RemoveAllUnreferencedMethodsOptimization.cs
+++ src/VarCode/Optimizer/Alpha/RemoveAllUnreferencedMethodsOptimization.cs
@@ -9,7 +9,7 @@ namespace Terumi.VarCode.Optimizer.Alpha
 		public bool Run(VarCodeStore store)
 		{
 			var entry = store.Entrypoint;
-			var references = AllReferences(entry);
+			var references = AllReferences(entry, entry.Tree.Code);
 
 			var deleteStructures = new List<VarCodeStructure>();
 
@@ -29,26 +29,75 @@ namespace Terumi.VarCode.Optimizer.Alpha
 			return deleteStructures.Count > 0;
 		}
 
-		private List<VarCodeId> AllReferences(VarCodeStructure investigate)
+		private List<VarCodeId> AllReferences(VarCodeStructure operate, List<VarInstruction> instructions)
 		{
 			var references = new List<VarCodeId>();
 
-			foreach (var loc in investigate.Tree.Code)
+			foreach (var instruction in instructions)
 			{
-				var expression = loc.TryExpression();
+				if (instruction is VarIf varIf)
+				{
+					foreach (var innerReference in AllReferences(operate, varIf.TrueBody))
+					{
+						if (!references.Contains(innerReference))
+						{
+							references.Add(innerReference);
+						}
+					}
+				}
+
+				if (instruction is VarMethodCall varMethodCall)
+				{
+					if (!references.Contains(varMethodCall.MethodCallVarExpression.MethodId))
+					{
+						references.Add(varMethodCall.MethodCallVarExpression.MethodId);
+					}
+
+					var method = operate.Store.GetStructure(varMethodCall.MethodCallVarExpression.MethodId);
+
+					if (method == null)
+					{
+						continue;
+					}
+
+					foreach (var innerReference in AllReferences(operate, method.Tree.Code))
+					{
+						if (!references.Contains(innerReference))
+						{
+							references.Add(innerReference);
+						}
+					}
+				}
+
+				var expression = instruction.TryExpression();
 
 				if (expression == default)
 				{
 					continue;
 				}
 
-				if (expression is MethodCallVarExpression methodCall
-					&& !references.Contains(methodCall.MethodId))
+				if (expression is MethodCallVarExpression methodCall)
 				{
-					references.Add(methodCall.MethodId);
+					if (!references.Contains(methodCall.MethodId))
+					{
+						references.Add(methodCall.MethodId);
+					}
+
+					var method = operate.Store.GetStructure(methodCall.MethodId);
+
+					if (method == null)
+					{
+						continue;
+					}
+
+					foreach (var innerReference in AllReferences(operate, method.Tree.Code))
+					{
+						if (!references.Contains(innerReference))
+						{
+							references.Add(innerReference);
+						}
+					}
 				}
-
-
 			}
 
 			return references;

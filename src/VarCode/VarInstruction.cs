37d99d46dd6d1b77b6486f940a63d7d4cc0d9654
SirJosh3917 <joshuaneedham@hotmail.com>
2019-11-14 19:36:44 -0500

    add simple VarCode instruction set for optimization

---
+++ src/VarCode/VarInstruction.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terumi.VarCode
+{
+	// unoptimized, simple instructions to use in the creation of a VarTree
+
+	public abstract class VarInstruction
+	{
+	}
+
+	public class VarAssignment : VarInstruction
+	{
+		public VarAssignment(int variableId, VarExpression value)
+		{
+			VariableId = variableId;
+			Value = value;
+		}
+
+		public int VariableId { get; }
+		public VarExpression Value { get; }
+	}
+
+	public class VarReturn : VarInstruction
+	{
+		public VarReturn(int id)
+		{
+			Id = id;
+		}
+
+		public int Id { get; }
+	}
+
+	public class VarMethodCall : VarInstruction
+	{
+		public VarMethodCall(int? variableId, MethodCallVarExpression methodCallVarExpression)
+		{
+			MethodCallVarExpression = methodCallVarExpression;
+			VariableId = variableId;
+		}
+
+		public int? VariableId { get; }
+		public MethodCallVarExpression MethodCallVarExpression { get; }
+	}
+
+	public abstract class VarExpression
+	{
+	}
+
+	public class ConstantVarExpression<T> : VarExpression
+	{
+		public ConstantVarExpression(T value)
+		{
+			Value = value;
+		}
+
+		public T Value { get; }
+	}
+
+	public class MethodCallVarExpression : VarExpression
+	{
+		public MethodCallVarExpression(int methodId, params int[] parameterVariables)
+		{
+			MethodId = methodId;
+			ParameterVariables = parameterVariables;
+		}
+
+		public int MethodId { get; }
+		public int[] ParameterVariables { get; }
+	}
+}
---
+++ src/VarCode/VarTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Terumi.VarCode
+{
+	public class VarTree
+	{
+		private int _counter;
+
+		public List<VarInstruction> Code { get; set; } = new List<VarInstruction>();
+
+		public int Push(string value)
+			=> AppendInstruction(id => new VarAssignment(id, new ConstantVarExpression<string>(value)));
+
+		public int Push(BigInteger value)
+			=> AppendInstruction(id => new VarAssignment(id, new ConstantVarExpression<BigInteger>(value)));
+
+		public int Push(bool value)
+			=> AppendInstruction(id => new VarAssignment(id, new ConstantVarExpression<bool>(value)));
+
+		/// <seealso cref="Call(int, int[])"/>
+		public void Execute(int methodId, params int[] vars)
+			=> Code.Add(new VarMethodCall(null, new MethodCallVarExpression(methodId, vars)));
+
+		/// <seealso cref="Execute(int, int[])"
+		public int Call(int methodId, params int[] vars)
+			=> AppendInstruction(id => new VarMethodCall(id, new MethodCallVarExpression(methodId, vars)));
+
+		public void Return(int variable)
+			=> Code.Add(new VarReturn(variable));
+
+		private int AppendInstruction(Func<int, VarInstruction> append)
+		{
+			var id = _counter++;
+			Code.Add(append(id));
+			return id;
+		}
+	}
+}

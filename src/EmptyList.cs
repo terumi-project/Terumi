52995473f6751a86986ce5de892a40017a57a2fa
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-02-08 21:38:42 -0500

    delete C# code

--- src/EmptyList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Terumi
-{
-	public static class EmptyList<T>
-	{
-		public static List<T> Instance { get; } = new List<T>(0);
-	}
-}
--- src/ParserConstants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Terumi
-{
-	public static class ParserConstants
-	{
-		public const int ParseNothingButSuccess = -1;
-	}
-}
